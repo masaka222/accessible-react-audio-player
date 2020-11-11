cd0c1053f735dbea16e213d722a0d1d191c29a66
Vasilis Georgoudis <masakaftw@hotmail.com>
2020-11-11 23:51:24 +0200

    play/pause button

--- src/components/audio-player.js
+++ src/components/audio-player.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 
 const AudioPlayer = ({
     src = 'https://res.cloudinary.com/jlengstorf/video/upload/v1604601320/boop-song.mp3',
     transcription = 'PEW PEW PEW PEWWWW'
 
 }) => {
+    const ref = useRef(null);
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [mediaTime, setMediaTime] = useState(0);
+    const [duration, setDuration] = useState(0);
+
+    const togglePlaying = (event) => {
+        setIsPlaying(!isPlaying);
+
+        const audioPlayer = ref.current;
+        isPlaying? audioPlayer.pause() : audioPlayer.play();
+    }
 return (
     <div>
-        <audio src={src} controls/>
+        <div>
+            <button onClick={togglePlaying}>{isPlaying? 'Pause': 'Play'}</button>
+            <span>elapsed time {mediaTime}</span>
+            <span>total duration {duration}</span>
+        </div>
+        <audio ref={ref} src={src} controls/>
         <p className='transcription'>{transcription}</p>
     </div>
 
